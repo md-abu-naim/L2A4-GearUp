e887725c4bd3a8f58e9a27506bed59216a1c000e
abunaim34 <farhanadnanfarabi@gmail.com>
2026-07-08 17:24:17 +0600

    category routes compelte

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { categroyController } from "./category.controller.js";
+import auth from "../../middleware/auth.js";
+import { UserRole } from "../../../generated/prisma/enums.js";
 
 const router = Router()
 
-router.post('/', categroyController.createCategory)
-router.get('/', categroyController.getAllCategories)
-router.get('/:id', categroyController.updateCategory)
-router.delete('/:id', categroyController.deleteCategory)
+router.post('/', auth(UserRole.ADMIN), categroyController.createCategory)
+router.get('/', auth(UserRole.ADMIN, UserRole.CUSTOMER, UserRole.PROVIDER), categroyController.getAllCategories)
+router.patch('/:id', auth(UserRole.ADMIN), categroyController.updateCategory)
+router.delete('/:id', auth(UserRole.ADMIN), categroyController.deleteCategory)
 
 export const categoryRouters = router
